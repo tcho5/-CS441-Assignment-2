2d14eaf6d6359fd8de815356503e0403ab337129
Taehyun Cho <taehyuncho@Taehyuns-MacBook-Air.local>
2019-02-11 10:40:08 -0500

    Implemented swiping left and right.

--- app/src/main/java/com/example/cs441_assignment2/MainActivity.java
+++ app/src/main/java/com/example/cs441_assignment2/MainActivity.java
@@ -2,17 +2,23 @@ package com.example.cs441_assignment2;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
-    Button startButton, button0, button1, button2, button3, button4, button5, button6, button7, button8, button9, button10, button11, button12, button13, button14, button15;
+    
+    private float x1,x2;
+    static final int MIN_DISTANCE = 150;
+    Button startButton, resetButton, button0, button1, button2, button3, button4, button5, button6, button7, button8, button9, button10, button11, button12, button13, button14, button15;
     int value = 2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         startButton = findViewById(R.id.startButton);
+        resetButton = findViewById(R.id.resetButton);
         button0 = findViewById(R.id.button0);
         button1 = findViewById(R.id.button1);
         button2 = findViewById(R.id.button2);
@@ -31,6 +37,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         button15 = findViewById(R.id.button15);
 
         startButton.setOnClickListener(this);
+        resetButton.setOnClickListener(this);
         button0.setOnClickListener(this);
         button1.setOnClickListener(this);
         button2.setOnClickListener(this);
@@ -47,7 +54,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         button13.setOnClickListener(this);
         button14.setOnClickListener(this);
         button15.setOnClickListener(this);
-
     }
 
     @Override
@@ -63,6 +69,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 break;
             case R.id.button1:
                 button0.setText(Integer.toString(value));
+                value *= 2;
                 break;
             case R.id.button2:
                 break;
@@ -92,11 +99,67 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 break;
             case R.id.button15:
             default:
+                value = 2;
                 break;
 
-
-
         }
+    }
+    @Override
+    public boolean onTouchEvent(MotionEvent event){
+        switch(event.getAction())
+        {
+            case MotionEvent.ACTION_DOWN:
+                x1 = event.getX();
+                break;
+            case MotionEvent.ACTION_UP:
+                x2 = event.getX();
+                float deltaX = x2 - x1;
 
+                if (Math.abs(deltaX) > MIN_DISTANCE)
+                {
+                    // Left to Right swipe action
+                    if (x2 > x1)
+                    {
+                        Toast.makeText(this, "Left to Right swipe [Next]", Toast.LENGTH_SHORT).show ();
+                    }
+
+                    // Right to left swipe action
+                    else
+                    {
+                        Toast.makeText(this, "Right to Left swipe [Previous]", Toast.LENGTH_SHORT).show ();
+                    }
+
+                }
+                else
+                {
+
+                    // consider as something else - a screen tap for example
+                }
+                break;
+        }
+        return super.onTouchEvent(event);
+
+//        int action = MotionEventCompat.getActionMasked(event);
+//
+//        switch(action) {
+//            case (MotionEvent.ACTION_DOWN) :
+//                Log.d(DEBUG_TAG,"Action was DOWN");
+//                return true;
+//            case (MotionEvent.ACTION_MOVE) :
+//                Log.d(DEBUG_TAG,"Action was MOVE");
+//                return true;
+//            case (MotionEvent.ACTION_UP) :
+//                Log.d(DEBUG_TAG,"Action was UP");
+//                return true;
+//            case (MotionEvent.ACTION_CANCEL) :
+//                Log.d(DEBUG_TAG,"Action was CANCEL");
+//                return true;
+//            case (MotionEvent.ACTION_OUTSIDE) :
+//                Log.d(DEBUG_TAG,"Movement occurred outside bounds " +
+//                        "of current screen element");
+//                return true;
+//            default :
+//                return super.onTouchEvent(event);
+//        }
     }
 }
